7b1c316556340206a772982ba17fe9d868bc4b76
[zmemer] <abdulaaqib4@gmail.com>
2024-03-21 09:01:02 +0000

    v61

--- app/page.tsx
+++ app/page.tsx
@@ -38,10 +38,10 @@ export default function Home() {
       </head>
       <body class="bg-gray-200">
         <header class="w-full p-4 bg-white flex justify-center items-center shadow-md">
-          <Image src="https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZudmC4uo7JnXvJeSsIwXqODG2IfP37ODdY6oodJ3EnQUBqSnlt1C_PJRJfDKYkE_7zhbSqn2h1TtVCLnaDEdOqrO-g=w1920-h863" alt="Burberry Logo" width={50} height={50} />
+          <Image src="https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZudmC4uo7JnXvJeSsIwXqODG2IfP37ODdY6oodJ3EnQUBqSnlt1C_PJRJfDKYkE_7zhbSqn2h1TtMrVCLnaDEdOqrO-g=w1920-h863" alt="Burberry Logo" width={50} height={50} />
         </header>
         <div class="w-full flex justify-center items-center mt-4">
-          <Image src="https://lh3.googleusercontent.com/fife/ALs6j_FoJ7RnTXAl97yP-ppUD2Fufweh1jQJsvbprSx-vV2z18_eEOsmgMYVo3ssqj0e3NaMDKW1oXPDBZPxmfIP6RkxhUVtvTMKKJXb4HJUJqOWFGm-u0iftFZSr7xphXdVRRqza0NCk1s4qiPNlwUCUSplr4QWXikTxaJGXiuql04DSdjHZ9NwEI759sMNhA-8C60H2gOokKlU3V4xyKkvgDJm5DUhl79xwi-n4KK32DLImfpvV4cESOdH-KJv_RsSedf_UlobXVQMkv-TIkqQOIVF4uYFAxdjCkboKXNVJvBXTUhS1xWOcCkaDyqak6xzRS2rbQja7x3pGNJS4VpZ_G96R_mlO2PyS9fHLHMUcdAJ_IAn6wVeIF8lcDw-3aQNPdlY5sYTAIJb34teKVzZWQ2d3nH3c2V5NESv5Gp8s3MXY5MUmrM-BAW3R_51tgiA8TusKmJkkz-__OWZWSSLAZKBMHiDfocOPkRi9BmFwNc3iprtqC95NXyg8aNyX_uUAS4v7Ywk9z_DTVc3owlAG7i4qbbJTD4-bnu5pwdOA0fT9nVU8LCW8mDv56P01158VUhL4Gc6VNzNN8Z-g-ZGReMBerUCdZIDM_qtm5PXb-6cMd1fk_X-bHyPQX3oOEuJUPg8ygjksJqWPRtIlaMCmfxznuSxDOofUalL3lYs55Ddohq2IjKSOcGfs6IRRZorHfgrjBVkG6zg9TK1d9EFgRFbqTLXrjXZ8RH8ANsTHfGcSp7UtEky4Xnf_kxYth7ZV8dCBemhELUctfg1Q06hel3SsfPl_KXEyTeMbM0ASrjFZgbEQP8exD_B79pvDJSd6YHsWt50Vj34YEqpRqD3RypXLgsFDhk-Le10i9XWCiQxxHs4rUtm1fxZ-BDFiK4r8zFGUpg4HLY7zUiT37NiYFUzFgawM0Cnzzqk3eqbStCuJOfwMD9_g5C5Np5G7-wgp-WiXbd6cAXx7wlgWEcJjDdsfYE5rnLxvqTMvRM=w1920-h863" alt="Your Image" width={800} height={600} />
+          <Image src="/your-image-path.jpg" alt="Your Image" width={800} height={600} />
         </div>
         <section id="full-height-section">
           <div class="product-card bg-white rounded-lg shadow-lg m-3">
